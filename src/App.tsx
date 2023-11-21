c7704012bdaa2dda7b72e806d16288ebddea2057
marlon <marlonsuyuc@gmail.com>
2023-11-21 16:47:18 -0600

    add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,33 @@
 import './App.css'
-import { Container, Stack, Typography } from '@mui/material'
-import { JavaScriptLogo } from './JavaScriptLogo'
-import { Start } from './Start'
+import { Container, Stack, Typography, useTheme } from '@mui/material'
+import { JavaScriptLogo } from './components/JavaScriptLogo'
+import { Start } from './components/Start'
 import { useQuestionsStore } from './store/questions'
-import { Game } from './Game'
+import { Game } from './components/Game'
+import useMediaQuery from '@mui/material/useMediaQuery'
 
 function App() {
   const questions = useQuestionsStore((state) => state.questions)
-  console.log(questions)
+  const theme = useTheme()
+  const medium = useMediaQuery(theme.breakpoints.up('md'))
 
   return (
     <>
       <main>
         <Container maxWidth='sm'>
-          <Typography variant='h2' component='h1'>
-            <Stack
-              direction='row'
-              gap={2}
-              alignItems='center'
-              justifyContent='center'
-            >
-              <JavaScriptLogo />
-              JavaScript Quizz
-            </Stack>
-            {questions.length === 0 && <Start />}
-            {questions.length > 0 && <Game />}
-          </Typography>
+          <Stack
+            direction='row'
+            gap={2}
+            alignItems='center'
+            justifyContent='center'
+          >
+            <JavaScriptLogo />
+            <Typography variant={medium ? 'h2' : 'h5'} component='h1'>
+              JavaScript Quiz
+            </Typography>
+          </Stack>
+          {questions.length === 0 && <Start />}
+          {questions.length > 0 && <Game />}
         </Container>
       </main>
     </>
---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+import { Button } from '@mui/material'
+import { useQuestionsStore } from '../store/questions'
+import { useQuestionsData } from '../hooks/useQuestionsData'
+
+export const Footer = () => {
+  const reset = useQuestionsStore((state) => state.reset)
+  const { correct, incorrect, unanswered } = useQuestionsData()
+
+  return (
+    <footer style={{ marginTop: '16px' }}>
+      <strong>{`✅ ${correct} correctas - ❌ ${incorrect} incorrectas - ❓ ${unanswered} sin responder`}</strong>
+      <div style={{ marginTop: '16px' }}>
+        <Button onClick={() => reset()}>Resetear juego</Button>
+      </div>
+    </footer>
+  )
+}
