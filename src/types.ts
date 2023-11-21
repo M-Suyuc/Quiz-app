89e89b48c9cf68a39341c1d14a35fda593df635f
marlon <marlonsuyuc@gmail.com>
2023-11-21 16:47:35 -0600

    add types

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,10 @@
 export interface Question {
   id: number
   question: string
+  code: string
   answers: string[]
   correctAnswer: number
-  code: string
-  userSelectedAnswers?: number
-  isCorrectUserAnswers?: boolean
+
+  isCorrectUserAnswer?: boolean
+  userSelectedAnswer?: number
 }
